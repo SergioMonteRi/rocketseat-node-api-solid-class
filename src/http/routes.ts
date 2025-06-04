4eb51aa72767069995a09ca7ee5e956893c3a81d
SergioMonteRi <sergioribeiropalermo@gmail.com>
2025-06-04 07:58:00 -0300

    feat(jwt): implement JWT with Fastify

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,9 +1,11 @@
 import { FastifyInstance } from 'fastify'
 
-import { authenticate, register } from './controllers'
+import { authenticate, profile, register } from './controllers'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register)
-
   app.post('/sessions', authenticate)
+
+  /* Authenticated */
+  app.get('/me', profile)
 }
